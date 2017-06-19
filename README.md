3ffe78571428ceaf262044dd14af5403f89794e7
petrusboniatus <pedrobuenisimo@gmail.com>
2017-06-19 12:59:18 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# wacomAreaChanger
-Wacom area automatic changer 
+# Wacom area automatic changer
+
+This script is a solution to the problem of the wacom awkward area on linux, specially for big screens,
+ multiple screen or wide screens.
+
+When it running you just have to **double click the mid mouse button** to center the area mapped on the mouse position,
+the area aspect ratio is automatically fixed, you only have to specify the part of the screen you want to use:
+ 
+ ```commandline
+    $ ./areaChanger <id wacom pen> <multiplier>
+```
+
+### How to use it
+
+0. Install wacom drivers: I used ```xsetwacom``` on 0.26.0 version 
+1. Install python the dependencies: 
+ ```commandline
+    $ apt-get install python-xlib
+    
+    $ pip install Xlib
+    $ pip install pymouse
+ ```
+ 2. Get the id of your wacom pen:
+  ```commandline
+    $ xsetwacom --list
+       
+    Wacom Intuos PT S Pen stylus    id: 8   type: STYLUS    
+    Wacom Intuos PT S Finger touch  id: 9   type: TOUCH     
+    Wacom Intuos PT S Pen eraser    id: 14  type: ERASER    
+    Wacom Intuos PT S Finger pad    id: 15  type: PAD 
+  ```
+ 3. Run the script, in this example I want the wacom area to occupy a fourth of the screen so the height multiplier
+ should be x2 (the width is set automatically depending on the tablet aspect ratio)
+ 
+ ```commandline
+    $ ./areaChanger 8 2
+```
+
+ 
---
+++ areaChanger.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+from pymouse import PyMouseEvent
+from pymouse import PyMouse
+import subprocess as tux
+import sys
+import shlex
+import time
+
+
+# Given a position on the screen this functions the wacom area on (mouse_x, mouse_y)
+def change_wacom_position(mouse_x, mouse_y):
+    wacom_pos_x = int(
+        -int(wacom_x) * int(mouse_x) * divisor * aspectRatioCorrection / displayResolution_X + int(wacom_x) / 2)
+    wacom_pos_y = int(-int(wacom_y) * int(mouse_y) * divisor / displayResolution_Y + int(wacom_y) / 2)
+
+    wacom_weigh = int(divisor * aspectRatioCorrection * int(wacom_x))
+    wacom_height = int(divisor * int(wacom_y))
+
+    tux.call(shlex.split("xsetwacom --set " + wacomID + " area " + str(wacom_pos_x) + " " + str(wacom_pos_y) + " " \
+                         + str(wacom_pos_x + wacom_weigh) + " " + str(wacom_pos_y + wacom_height)))
+
+
+lastClick = 0
+
+
+def is_double_click():
+    global lastClick
+    new_click = time.time()
+    delta = new_click - lastClick
+    lastClick = new_click
+    if delta < 0.6:
+        return True
+    else:
+        return False
+
+
+class MouseListener(PyMouseEvent):
+    def __init__(self):
+        PyMouseEvent.__init__(self)
+
+    def click(self, x, y, button, press):
+        if press and button == 3 and is_double_click():
+            time.sleep(0.2)
+            change_wacom_position(x, y)
+
+
+if len(sys.argv) != 3:
+    print("./areaChanger.py < wacom tablet id > < division ratio of the screen >")
+    exit(1)
+
+# It's the global party now :)
+
+wacomID = sys.argv[1]
+divisor = float(sys.argv[2])
+
+mouse = PyMouse()
+displayResolution_X, displayResolution_Y = mouse.screen_size()
+
+tux.call(shlex.split("xsetwacom --set " + wacomID + " ResetArea"))
+
+_, _, wacom_x, wacom_y = tux.check_output(shlex.split(" xsetwacom --get " + wacomID + " area")).split(" ")
+aspectRatioCorrection = (float(displayResolution_X) / float(displayResolution_Y)) / (float(wacom_x) / float(wacom_y))
+
+C = MouseListener()
+C.run()
